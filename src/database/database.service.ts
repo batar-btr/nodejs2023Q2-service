8645c78be8f662deeec031ccb2e0fe1fff2a0d04
batar-btr <btrn86@gmail.com>
2023-07-28 23:25:06 +0300

    feat: create artist routes & artists db

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Users } from './users';
-import { Tracks } from './tracks';
+import { Users } from './entities/users';
+import { Tracks } from './entities/tracks';
+import { Artists } from './entities/artists';
 
 @Injectable()
 export class DatabaseService {
   users: Users;
   tracks: Tracks;
+  artists: Artists;
   constructor() {
     this.users = new Users();
     this.tracks = new Tracks();
+    this.artists = new Artists();
   }
 }
