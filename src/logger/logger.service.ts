c1e96858f55e60c77f7ff622d96815823b4b646e
Vadzim Antonau <vanet@tut.by>
2023-08-15 15:23:32 +0300

    feat: rebuild custom-logging service

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,23 +1,26 @@
-import { LoggerService } from '@nestjs/common';
+import { ConsoleLogger, LoggerService, Injectable } from '@nestjs/common';
 
+@Injectable()
 export class MyCustomLoggingService implements LoggerService {
+  private readonly logger = new ConsoleLogger('Custom-Logger');
+
   log(message: string) {
-    console.log(`[Custom Logging - Log] - ${message}`);
+    this.logger.log(`[Custom Log] - ${message}`);
   }
 
   error(message: string) {
-    console.error(`[Custom Logging - Error] - ${message}`);
+    this.logger.error(`[Custom Error] - ${message}`);
   }
 
   warn(message: string) {
-    console.warn(`[Custom Logging - Warn] - ${message}`);
+    this.logger.warn(`[Custom Warn] - ${message}`);
   }
 
   debug?(message: string) {
-    console.debug(`[Custom Logging - Debug] - ${message}`);
+    this.logger.debug(`[Custom Debug] - ${message}`);
   }
 
   verbose?(message: string) {
-    console.log(`[Custom Logging - Verbose] - ${message}`);
+    this.logger.verbose(`[Custom Verbose] - ${message}`);
   }
 }
